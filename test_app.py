2fa828f8958724d90dc937cac35de98e7e1064d0
psai0011 <panthaganisai123@gmail.com>
2025-05-03 18:18:39 +0530

    Fix status code assertion in test

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from app import app
 def test_home():
     response = app.test_client().get("/")
 
-    assert response.status==200
+    assert response.status_code == 200
     assert response.data == "Hello world!"
